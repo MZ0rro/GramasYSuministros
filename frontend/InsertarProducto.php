8233fa16e9a8440f0af3b2a1cbd0f4ddddc7a657
Andresmunoz12 <amunozlombana@gmail.com>
2025-11-04 13:23:49 -0500

    insertar y inventario enlazados con css

--- frontend/InsertarProducto.php
+++ frontend/InsertarProducto.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="InsertarProducto.css" />
+    <link rel="stylesheet" href="estilos/InsertarProducto.css" />
     <link
       rel="icon"
       type="image/png"
--- frontend/InventarioGrama.php
+++ frontend/InventarioGrama.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="InventarioGrama.css" />
+    <link rel="stylesheet" href="estilos/InventarioGrama.css" />
     <link
       rel="icon"
       type="image/png"
