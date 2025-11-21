9d7f802f048575a2c619ac87d334a02b1e2ab4df
CrashhSus <santidavila233@gmail.com>
2025-11-21 00:19:22 -0500

    Agrego nueva vista: Catalogo V1 - Santiago

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
-import Catalogo from "./pages/Catalogo.jsx";
-import EliminarProducto from "./pages/EliminarProducto.jsx";
+import Catalogo from "./pages/Catalogo.jsx"
 
 export default function App() {
   return (
@@ -13,7 +12,6 @@ export default function App() {
       <Route path="/register" element={<Register />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/catalogo" element={<Catalogo />} />
-      <Route path="/EliminarProducto" element={<EliminarProducto />} />
     </Routes>
   );
 }
