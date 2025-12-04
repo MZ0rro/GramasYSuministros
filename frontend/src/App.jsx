1b97ee425d2ffc76ac7673e35152eac9d20f0957
JhojanMoreno <jhojanmorenosb@gmail.com>
2025-12-04 11:46:28 -0500

    cargo version actualizada a mi rama.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,10 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
-import Catalogo from "./pages/Catalogo.jsx"
+import Catalogo from "./pages/Catalogo.jsx";
+import { Routes, Route } from 'react-router-dom';
+import AdminDashboard from './pages/AdminDashboard';
+import Reportes from './pages/reportes';
 
 export default function App() {
   return (
@@ -12,6 +15,8 @@ export default function App() {
       <Route path="/register" element={<Register />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/catalogo" element={<Catalogo />} />
+      <Route path="/AdminDashboard" element={<AdminDashboard />} />
+      <Route path="/reportes" element={<Reportes />} />
     </Routes>
   );
 }
